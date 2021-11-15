9280d96ecd214f2c8e9cbc02f710412e0ee678f0
Anthony McAuliffe <54680824+am17145@users.noreply.github.com>
2021-11-15 16:37:00 -0500

    Add files via upload

--- TrafficDesityML.py
+++ TrafficDesityML.py
@@ -2,17 +2,29 @@ import matplotlib.pyplot as plt
 import numpy
 import pandas as pd
 from matplotlib import pyplot
+from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+def createmodel(id, city):
+    filename = 'DatabyCities/'+city
+    columns = ['day','interval','detid','flow','occ','error','city','speed']
+    df = pd.read_csv(filename,header=0,names=columns)
+    groupedbyid = df.groupby('detid')
+    df_new = groupedbyid.get_group(id)
+    flow = df_new['flow']
+    time = []
+    x = 0
+    for row in df_new.itertuples():
+        if row.day == '2017-05-06':
+            time.append(row.interval)
+        else:
+            temp = row.interval + (86100 * x)
+            time.append(temp)
+        x+=1
+    model = numpy.poly1d(numpy.polyfit(time, flow, 9))
+    line = numpy.linspace(10, 1722000, 300)
+    plt.scatter(time, flow)
+    plt.plot(line, model(line))
+    plt.show()
 
-filename = 'DatabyCities/augsburg'
-columns = ['day','interval','detid','flow','occ','error','city','speed']
-df = pd.read_csv(filename,header=0,names=columns)
-groupedbyid = df.groupby('detid')
-df_new = groupedbyid.get_group('06.X-2li')
-model = numpy.poly1d(numpy.polyfit(df_new['interval'],df_new['flow'],3))
-line = numpy.linspace(20,90000,175)
-plt.scatter(df_new['interval'],df_new['flow'])
-plt.plot(line,model(line))
-plt.show()
 
-
+createmodel('06.X-2r','augsburg')
