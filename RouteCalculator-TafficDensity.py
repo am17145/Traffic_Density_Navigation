1baeaaa15a6c549369db756b7c29553cefc6115f
Anthony McAuliffe <54680824+am17145@users.noreply.github.com>
2021-12-01 16:42:27 -0500

    Add files via upload

--- RouteCalculator-TafficDensity.py
+++ RouteCalculator-TafficDensity.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 from libpysal.weights import lat2W
+from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
 #This creates a new column know as new id to help simplify the traversal aspect ex. detid 06.X-2li -> 1
@@ -37,18 +38,18 @@ def GetFlow(id,currentinteval):
     interval = []
     count = 0
     for row in df_new.itertuples():
-        interval.append(row.interval)
+        interval.append(row.interval + (86100 * count))
         flow.append(row.flow)
         count+=1
 
     #This is the formula for sinusoidal equations
-    def function(x, A, b, phi, c):
-        y = A * np.sin(b * x + phi) + c
+    def sinfunction(x, A, B, C, D):
+        y = A * np.sin(B * x + C) + D
         return y
 
     #This finds the values needed in order to best fit the our model
     initial = (15, 100000, 5, 20)
-    popt, pcov = curve_fit(function, interval, flow,initial)
+    popt, pcov = curve_fit(sinfunction, interval, flow,initial)
 
     #This calculates the weight using of the selected id at the current moment and the sinusoidal regression
     weight = popt[0]*np.sin(popt[1]*currentinteval+popt[2])+popt[3]
@@ -70,9 +71,10 @@ def TreeSearch(startID,endID):
         neighbors = [key for key, value in g[currentID].items()]
         min = float('inf')
         for x in neighbors:
-            if (x == endID or GetFlow(x,GetCurrentInterval()) < min):
+            if (x == endID) or (GetFlow(x,GetCurrentInterval()) < min):
+                min = GetFlow(x,GetCurrentInterval())
                 currentID = x
         path.append(currentID)
     return path
 #Launch using the two command line prompts for the start and end ids
-TreeSearch(sys.argv[1],sys.argv[2])
+print(TreeSearch(int(sys.argv[1]),int(sys.argv[2])))
